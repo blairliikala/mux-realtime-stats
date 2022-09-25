033ee4160a9d2c04f3016c143588f2ce60051af6
Blair <blair.liikala@gmail.com>
2022-09-25 17:27:43 -0500

    Update realtimeviews.js

--- realtimeviews.js
+++ realtimeviews.js
@@ -418,8 +418,8 @@ export class MuxRealtimeViews extends HTMLElement {
   parseJwt (TOKEN) {
     let jsonPayload = '';
     try {
-      let base64Url = TOKEN.split('.')[1];
-      let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      const base64Url = TOKEN.split('.')[1];
+      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
       jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
           return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
       }).join(''));
