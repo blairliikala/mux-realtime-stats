98159696d1ac01bd262a19269894f7b7e4071258
Blair <blair.liikala@gmail.com>
2023-05-15 23:08:54 -0500

    Update realtimeviews.js

--- src/realtimeviews.js
+++ src/realtimeviews.js
@@ -471,19 +471,21 @@ export class MuxRealtimeViews extends HTMLElement {
   }
 
   #getRelativeTimeDistance(d1, d2 = new Date()) {
+    if (!d1) return '';
     const units = {
-      year: 24 * 60 * 60 * 1000 * 365,
-      month: 24 * 60 * 60 * 1000 * 365 / 12,
-      day: 24 * 60 * 60 * 1000,
-      hour: 60 * 60 * 1000,
+      year  : 24 * 60 * 60 * 1000 * 365,
+      month : 24 * 60 * 60 * 1000 * 365/12,
+      day   : 24 * 60 * 60 * 1000,
+      hour  : 60 * 60 * 1000,
       minute: 60 * 1000,
       second: 1000
-    };
-    const rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' });
+    }
+    
     const elapsed = d1 - d2;
-    for (var u in units)
-      if (Math.abs(elapsed) > units[u] || u == 'second')
-        return rtf.format(Math.round(elapsed / units[u]), u);
+    const unit = Object.keys(units).find(u => (Math.abs(elapsed) > units[u] || u === 'second'));
+  
+    const rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' })
+    return rtf.format(Math.round(elapsed/units[unit]), unit);
   }
 
   #event(name, details, object) {
